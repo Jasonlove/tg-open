abdd2f1fccbe76a23d43252181dd41df02fbabb0
jiajia.xiao <jiajia.xiao@luckincoffee.com>
2019-04-26 09:31:15 +0800

    初始化项目

---
+++ src/main/java/com/jinkuangkj/open/TgOpenApplication.java
@@ -0,0 +1,13 @@
+package com.jinkuangkj.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgOpenApplication.class, args);
+	}
+
+}
