d375f6e43c452b2f0f6c1c8b83134f366b5e49f6
jiajia.xiao <jiajia.xiao@luckincoffee.com>
2019-05-03 15:49:39 +0800

    修改页面

--- src/main/java/com/jinkuangkj/open/controller/AdminController.java
+++ src/main/java/com/jinkuangkj/open/controller/AdminController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jinkuangkj.open.model.Activity;
 import com.jinkuangkj.open.service.ActivityService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("admin")
 public class AdminController {
@@ -31,6 +35,12 @@ public class AdminController {
 		List<Activity> list = activityService.getList();
 		model.addAttribute("actList", list);
 		return "admin/activity/index";
+	}
+	
+	@PostMapping("/activity/save")
+	public String saveActivity(Activity activity) {
+		activityService.saveActivity(activity);
+		return "redirect:/admin/activity/index";
 	}
 	
 	
