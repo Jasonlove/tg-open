ea2d29e89deeec35e1e6b35e774255fb19e0ae65
jiajia.xiao <jiajia.xiao@luckincoffee.com>
2019-05-04 22:12:58 +0800

    修改时间

--- src/main/resources/static/open/js/myjs.js
+++ src/main/resources/static/open/js/myjs.js
@@ -1,6 +1,9 @@
 function FreshTime()
 {
-    var endtime=new Date("2019/5/15,00:00:00");//结束时间
+	var time=$("#time_x").val();
+	//alert(time)
+    var endtime=new Date(time);//结束时间
+    //alert(endtime)
     var nowtime = new Date();//当前时间
     var lefttime= parseInt((endtime.getTime()-nowtime.getTime())/1000); 
     d=parseInt(lefttime/3600/24); ;
@@ -12,8 +15,11 @@ function FreshTime()
     $("#LeftTime").html(d+"<em>天</em>"+h+"<em>小时</em>"+m+"<em>分</em>"+s+"<em>秒</em>");
    
     if(lefttime <0){
-    document.getElementById("LeftTime").innerHTML="团购已结束";
-    clearInterval(sh);
+	    document.getElementById("LeftTime").innerHTML="团购已结束";
+	    clearInterval(sh);
+	    $(".con_bottom").css("display","none");
+	    $(".con_bottom .btn_left").css("display","none");
+	    $(".con_bottom .btn_right").css("display","none");
     }
 }
 FreshTime()
