709b6f792f093a7dcd299ef3ec375988852d7d99
jiajia.xiao <jiajia.xiao@luckincoffee.com>
2019-05-07 09:38:58 +0800

    添加上传接口

---
+++ src/main/java/com/jinkuangkj/open/controller/ComponentController.java
@@ -0,0 +1,15 @@
+package com.jinkuangkj.open.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("component")
+public class ComponentController {
+	
+	@RequestMapping("upload")
+	public void upload() {
+		
+	}
+
+}
