054a0b27275931aa39a91f235bc95edb6a4762a0
jiajia.xiao <jiajia.xiao@luckincoffee.com>
2019-05-25 17:00:23 +0800

    添加拦截器

--- src/main/java/com/jinkuangkj/open/config/AuthInterceptor.java
+++ src/main/java/com/jinkuangkj/open/config/AuthInterceptor.java
@@ -1,13 +1,19 @@
 package com.jinkuangkj.open.config;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.jinkuangkj.open.mapper.AuthDao;
+import com.jinkuangkj.open.model.Admin;
+import com.jinkuangkj.open.model.Auth;
 import com.jinkuangkj.open.model.result.Menu;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,22 +23,33 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthInterceptor implements HandlerInterceptor{
 	
 	
+	@Autowired
+	AuthDao authDao;
+	
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
-		log.info("权限拦截器拦截");
+		String uri = request.getRequestURI();
+		log.info("权限拦截器拦截-请求地址:{}",uri);
 		HttpSession session = request.getSession();
-		Object account = session.getAttribute("admin");
+		Admin  account = (Admin) session.getAttribute("admin");
 		if(account == null) {
 			response.sendRedirect(request.getContextPath()+"/openAdmin/login");
 			return false;
 		}
-		
-		Menu menu = (Menu) session.getAttribute("menu");
-		
-		
+		boolean flag = false;
+		List<Auth> list = authDao.listByRoleId(account.getRoleId());
+		for (Auth auth : list) {
+			if(uri.contains(auth.getUrl())) {
+				flag = true;
+				break;
+			}
+		}
+		if(!flag) {
+			response.sendRedirect(request.getContextPath()+"/openAdmin/login");
+			return false;
+		}
 		return true;
 	}
 
